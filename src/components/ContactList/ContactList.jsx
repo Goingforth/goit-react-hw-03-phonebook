824ef73c003ab6f42e6400f9f5747b4ad8d0003d
Goingforth <114081671+Goingforth@users.noreply.github.com>
2023-05-09 09:52:10 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import { List, Message } from './List.styled';
+import ContactItem from 'components/App/ContactItem/ContactItem';
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <List>
+      {contacts.length > 0 ? (
+        contacts.map(({ id, name, number }) => (
+          <ContactItem
+            key={id}
+            id={id}
+            name={name}
+            number={number}
+            onDeleteContact={onDeleteContact}
+          />
+        ))
+      ) : (
+        <Message>No contacts in phonebook.</Message>
+      )}
+    </List>
+  );
+};
+export default ContactList;
